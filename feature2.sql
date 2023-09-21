0e52e7e208c7ddc0156e169dd0e1174316ae1fb8
johnmcg04 <john.mcglinchey@kainos.com>
2023-09-21 13:56:25 +0100

    Feature 2 updated

--- feature2.sql
+++ feature2.sql
@@ -8,6 +8,22 @@ CREATE TABLE Employee(
     businessSector VARCHAR(30)
 );
 
+CREATE TABLE Employee_Financials (
+    financeId INT AUTO_INCREMENT PRIMARY KEY,
+    employeeId INT,
+    bankAccountNum VARCHAR(20),
+    nationalInsuranceNum VARCHAR(10),
+    CONSTRAINT fk_employee FOREIGN KEY (employeeId) REFERENCES Employee(employeeId),
+	CONSTRAINT check_ni_format CHECK (
+        LENGTH(nationalInsuranceNum) = 9 AND
+        SUBSTRING(nationalInsuranceNum, 1, 1) BETWEEN 'A' AND 'Z' AND
+        SUBSTRING(nationalInsuranceNum, 2, 1) BETWEEN 'A' AND 'Z' AND
+        SUBSTRING(nationalInsuranceNum, 3, 6) BETWEEN '0' AND '9' AND
+        SUBSTRING(nationalInsuranceNum, 9, 1) BETWEEN 'A' AND 'Z'
+        )
+        
+);
+
 INSERT INTO Employee (BusinessSector, FirstName, Surname) -- inputting data into the Employee table
 VALUES
     ('Delivery', 'John', 'McGlinchey'); -- inserting a delivery employee
