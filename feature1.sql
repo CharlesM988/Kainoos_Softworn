77959f18c515fe3655c66a30963b8e5920099437
Breanna-loughrey <145446184+Breanna-loughrey@users.noreply.github.com>
2023-09-21 09:59:17 +0100

    Update feature1.sql

--- feature1.sql
+++ feature1.sql
@@ -3,14 +3,14 @@ USE KainoosProjectTeam4_JohnMc;
 CREATE TABLE Employee(
 	employeeId INT AUTO_INCREMENT PRIMARY KEY,
 	firstName VARCHAR(80),
-    surname VARCHAR(80),
-    jobTitle VARCHAR(30),
-    businessSector VARCHAR(30)
+	surname VARCHAR(80),
+	jobTitle VARCHAR(30),
+	businessSector VARCHAR(30)
 );
 
 CREATE TABLE Sales_Employees(
 	employeeId INT,
-    commission_Rate Decimal(5,2),
+	commission_Rate Decimal(5,2),
     CONSTRAINT chkCommissionRate CHECK (commission_Rate >= 0 AND commission_Rate <= 100),
 	CONSTRAINT fk_employeeSales FOREIGN KEY (employeeId) REFERENCES Employee(employeeId)
 );
