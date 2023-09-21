c189978e7c9851163ac6368163732a2e193748f8
johnmcg04 <john.mcglinchey@kainos.com>
2023-09-21 18:16:24 +0100

    feature 13 updated

---
+++ feature13.sql
@@ -0,0 +1,15 @@
+use KainoosProjectTeam4_JohnMc;
+
+SELECT 
+    CONCAT(E.firstName, ' ', E.surname) AS `Sales employee name`,
+    SUM(SE.commission_Rate * P.projectValue) AS `Commission made`
+FROM 
+    Employee AS E
+JOIN 
+    Sales_Employees AS SE ON SE.employeeId = E.employeeId
+JOIN 
+    Client AS C ON C.employeeId = E.employeeId
+JOIN 
+    Project AS P ON P.clientId = C.clientId
+GROUP BY 
+    E.employeeId, E.firstName, E.surname; 
