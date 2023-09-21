bffe754f882a27103e5b60ce553fb0b890c91de8
johnmcg04 <john.mcglinchey@kainos.com>
2023-09-21 09:26:34 +0100

    feature1

---
+++ feature1.sql
@@ -0,0 +1,16 @@
+use KainoosProjectTeam4_JohnMc;
+
+CREATE TABLE Employee(
+	employeeId INT AUTO_INCREMENT PRIMARY KEY,
+	firstName VARCHAR(80),
+    surname VARCHAR(80),
+    jobTitle VARCHAR(30),
+    businessSector VARCHAR(30)
+);
+
+CREATE TABLE Sales_Employees(
+	employeeId INT,
+    commission_Rate Decimal(5,2),
+    CONSTRAINT chkCommissionRate CHECK (commission_Rate >= 0 AND commission_Rate <= 100),
+	CONSTRAINT fk_employeeSales FOREIGN KEY (employeeId) REFERENCES Employee(employeeId)
+);
