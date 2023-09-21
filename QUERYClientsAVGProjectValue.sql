199ff80f9b1303ef1586661de0513bc4ace93075
Breanna-loughrey <145446184+Breanna-loughrey@users.noreply.github.com>
2023-09-21 16:02:59 +0100

    Update QUERYClientsAVGProjectValue.sql

--- QUERYClientsAVGProjectValue.sql
+++ QUERYClientsAVGProjectValue.sql
@@ -1,4 +1,4 @@
-SELECT name AS `Client Name`, AVG(ProjectValue)
+SELECT name AS `Client Name`, AVG(ProjectValue) AS `Average Project Value`
 FROM Client 
 JOIN Project USING (ClientId)
 GROUP BY `Client Name`;
